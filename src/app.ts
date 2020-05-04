5fb48880361d07776c52e51ea5caf769effdd5b1
Bibiana <souzabibiana@hotmail.com>
2020-05-04 10:25:24 -0700

    Add files via upload

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import { ProjectInput } from "components/ProjectInput";
+import { ProjectList } from "components/ProjectList";
+
+export module App {
+  export const proj = new ProjectInput.ProjectInput(); //creates a variable and instantiates a ProjectInput object
+  export const activeProjList = new ProjectList.ProjectList("active"); //instantiates the project list
+  export const finishedProjList = new ProjectList.ProjectList("finished"); //instantiates the project list
+}
