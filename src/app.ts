7f49bf8e211b40a83c908b10b98ecd8b7b4ca34b
Bibiana <souzabibiana@hotmail.com>
2020-05-04 10:23:53 -0700

    Delete app.ts

--- src/app.ts
+++
@@ -1,8 +0,0 @@
-import { ProjectInput } from "./components/ProjectInput";
-import { ProjectList } from "./components/ProjectList";
-
-export module App {
-  export const proj = new ProjectInput.ProjectInput(); //creates a variable and instantiates a ProjectInput object
-  export const activeProjList = new ProjectList.ProjectList("active"); //instantiates the project list
-  export const finishedProjList = new ProjectList.ProjectList("finished"); //instantiates the project list
-}
