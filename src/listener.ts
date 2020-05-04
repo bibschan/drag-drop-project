dfbb24b3f838619fad8b12a808251f430fe7f999
Bibiana <souzabibiana@hotmail.com>
2020-05-04 10:23:11 -0700

    Delete listener.ts

--- src/listener.ts
+++
@@ -1,6 +0,0 @@
-import { Project } from "Project";
-
-export module Listener {
-  //custom type - encode a function type with no implementation
-  export type Listener = (items: Project.Project[]) => void;
-}
